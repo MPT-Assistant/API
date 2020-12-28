6cd77dd881321d851db9863782e23e4e2d462289
rus_anonym <alexandrsemin2033@gmail.com>
2020-12-28 22:18:44 +0300

    update

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,11 @@ export interface configInterface {
 	secretKey: string;
 	mongoDB: string;
 }
+
+export interface API {
+	token: string;
+}
+
+export interface APIGetGroupSchedule extends API {
+	id: string;
+}
