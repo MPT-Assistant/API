a5ec9bb329c1cd0dc047bbbf0098ec399b411bda
RusAnonym <alexandrsemin2033@gmail.com>
2021-03-07 16:10:28 +0300

    utils class has private constructor

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,4 +2,7 @@ import MPT from "./utils/mpt";
 
 export default class Utils {
 	public static MPT: MPT = new MPT();
+
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	private constructor() {}
 }
