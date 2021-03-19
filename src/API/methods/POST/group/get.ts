e9070f75474ffa1e65d00a481d7af10407e161cf
RusAnonym <alexandrsemin2033@gmail.com>
2021-03-19 23:27:57 +0300

    add group.get method

---
+++ src/API/methods/POST/group/get.ts
@@ -0,0 +1,37 @@
+import { RouteShorthandOptions } from "fastify/types/route";
+import server from "../../../main";
+import InternalUtils from "../../../../lib/utils";
+
+interface Body {
+    name: string;
+}
+
+const opts: RouteShorthandOptions = {
+    schema: {
+        body: {
+            name: { type: "string" },
+        },
+    },
+    preValidation: (request, reply, done) => {
+        const { name } = request.body as Body;
+        done(!name || name === "" ? new Error("Group ID not specified") : undefined);
+    },
+};
+
+server.post<{
+    Body: Body;
+}>("/api/group.get", opts, async (request) => {
+    const selectedGroupName = request.body.name;
+
+    const findGroup = InternalUtils.MPT.data.groups.find(
+        (group) => group.name.toLowerCase() === selectedGroupName,
+    );
+
+    if (!findGroup) {
+        throw new Error("Group not found");
+    } else {
+        return {
+            response: findGroup,
+        };
+    }
+});
