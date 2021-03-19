3a2f343837fcedd1c83e557cf2255ba79e767ddf
rus_anonym <alexandrsemin2033@gmail.com>
2021-03-19 18:11:01 +0300

    final

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import "./lib/core";
-import InternalUtils from "./lib/utils";
+// import InternalUtils from "./lib/utils";
 
 // import * as utils from "rus-anonym-utils";
 
 // utils.array.clone
 
 (async function () {
-	// console.log(await InternalUtils.MPT.updateData());
+	// console.log(
+	// 	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	// 	require(`fs`).writeFileSync(
+	// 		"out.json",
+	// 		JSON.stringify(await InternalUtils.MPT.parseLessons()),
+	// 	),
+	// );
 })();
