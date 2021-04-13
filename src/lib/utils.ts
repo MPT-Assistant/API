a68fe96240da8ab0e99c8e5f132e14366d10bc31
RusAnonym <alexandrsemin2033@gmail.com>
2021-04-14 02:12:05 +0300

    add miniapp.getUserData method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,12 +12,57 @@ export default class Utils {
 		database: "API",
 	});
 
-	public VK_Bot_DB = new VK_Bot_DB({
+	public static VK_Bot_DB = new VK_Bot_DB({
 		url: config.mongo.address,
 		login: config.mongo.login,
 		password: config.mongo.password,
 		database: "vk",
 	});
+
+	public static verifyLaunchParams(query: string): boolean {
+		let sign;
+		const hashQueryParams: {
+			key: string;
+			value: string;
+		}[] = [];
+
+		const processQueryParam = (key: string, value: string) => {
+			if (typeof value === "string") {
+				if (key === "sign") {
+					sign = value;
+				} else if (key.startsWith("vk_")) {
+					hashQueryParams.push({ key, value });
+				}
+			}
+		};
+
+		const formattedQuery = query.startsWith("?") ? query.slice(1) : query;
+
+		for (const param of formattedQuery.split("&")) {
+			const [key, value] = param.split("=");
+			processQueryParam(key, value);
+		}
+
+		if (!sign || hashQueryParams.length === 0) {
+			return false;
+		}
+
+		const queryString = hashQueryParams
+			.sort((a, b) => a.key.localeCompare(b.key))
+			.reduce((acc, { key, value }, idx) => {
+				return (
+					acc + (idx === 0 ? "" : "&") + `${key}=${encodeURIComponent(value)}`
+				);
+			}, "");
+
+		const paramsHash = CryptoJS.HmacSHA256(queryString, config.vk.secretKey)
+			.toString(CryptoJS.enc.Base64)
+			.replace(/\+/g, "-")
+			.replace(/\//g, "_")
+			.replace(/=$/, "");
+
+		return paramsHash === sign;
+	}
 
 	private constructor() {
 		return this;
--- src/www/API/methods/POST/miniapp/getUserData.ts
+++ src/www/API/methods/POST/miniapp/getUserData.ts
@@ -1,23 +1,52 @@
-// import { RouteShorthandOptions } from "fastify/types/route";
-// import server from "../../../../../lib/server";
-// import InternalUtils from "../../../../../lib/utils";
+import { RouteShorthandOptions } from "fastify/types/route";
+import server from "../../../../../lib/server";
+import InternalUtils from "../../../../../lib/utils";
 
-// interface Body {
-// 	id: number | number[];
-// }
+interface Body {
+	id: number | number[];
+	sign: string;
+}
 
-// const opts: RouteShorthandOptions = {
-// 	schema: {
-// 		body: {
-// 			id: { type: "string" },
-// 		},
-// 	},
-// 	preValidation: (request, reply, done) => {
-// 		const { id } = request.body as Body;
-// 		done(!id || id <= 0 ? new Error("ID not specified") : undefined);
-// 	},
-// };
+const opts: RouteShorthandOptions = {
+	schema: {
+		body: {
+			id: { type: "string" },
+			sign: { type: "string" },
+		},
+	},
+	preValidation: (request, reply, done) => {
+		const { id, sign } = request.body as Body;
+		if (!sign || InternalUtils.verifyLaunchParams(sign)) {
+			new Error("Sign not specified or invalid");
+		}
+		if (!id || id <= 0) {
+			new Error("ID not specified");
+		}
+		done();
+	},
+};
 
-// server.post<{
-// 	Body: Body;
-// }>("/api/miniapp.getUserdata", opts, async (request) => {});
+server.post<{
+	Body: Body;
+}>("/api/miniapp.getUserdata", opts, async (request) => {
+	if (typeof request.body.id === "number") {
+		request.body.id = [request.body.id];
+	}
+	if (request.body.id.length > 50) {
+		throw new Error("To many users");
+	}
+
+	const selectedUsers = await InternalUtils.VK_Bot_DB.models.user.find({
+		id: {
+			$in: request.body.id.map((x) => Number(x)),
+		},
+	});
+
+	return selectedUsers.map((user) => {
+		return {
+			id: user.id,
+			group: user.group,
+			reg_date: user.reg_date,
+		};
+	});
+});
